f6be3686f501544622303205ad6d8e726b2cc00c
Carlos Buitrago <letscatchupagain@gmail.com>
2024-12-24 15:59:49 +0000

    Change store layout. Create Posts components to show as home page with pics subreddit.

--- src/store/redditSlice.js
+++ src/store/redditSlice.js
@@ -1,38 +1,101 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getPostComments, getSubredditPosts, getSubreddits } from '../api/api';
 
 const initialState = {
-  posts: [],
-  status: 'idle',
-  error: null,
+  subreddits: {
+    list: [],
+    status: 'idle',
+    error: null,
+  },
+  posts: {
+    bySubreddit: {},
+    status: 'idle',
+    error: null,
+  },
+  comments: {
+    byPostId: {},
+    status: 'idle',
+    error: null,
+  },
+  selectedSubreddit: '/r/pics',
 };
 
 export const fetchPosts = createAsyncThunk(
   'reddit/fetchPosts',
   async (subreddit) => {
-    const response = await fetch(`https://www.reddit.com${subreddit}.json`);
-    const data = await response.json();
-    return data.data.children.map((post) => post.data);
+    const posts = await getSubredditPosts(subreddit);
+    return { subreddit, posts };
+  },
+);
+
+export const fetchSubreddits = createAsyncThunk(
+  'reddit/fetchSubreddits',
+  async () => {
+    return await getSubreddits();
+  },
+);
+
+export const fetchComments = createAsyncThunk(
+  'reddit/fetchComments',
+  async ({ permalink, postId }) => {
+    const comments = await getPostComments(permalink);
+    return { postId, comments };
   },
 );
 
 const redditSlice = createSlice({
   name: 'reddit',
   initialState,
-  reducers: {},
+  reducers: {
+    setSelectedSubreddit(state, action) {
+      state.selectedSubreddit = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder
+      //fetch posts
       .addCase(fetchPosts.pending, (state) => {
         state.status = 'loading';
       })
       .addCase(fetchPosts.fulfilled, (state, action) => {
+        const { subreddit, posts } = action.payload;
+        state.posts.bySubreddit[subreddit] = posts;
         state.status = 'succeded';
-        state.posts = action.payload;
+        state.posts.error = null;
       })
       .addCase(fetchPosts.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
+        state.posts.status = 'failed';
+        state.posts.error = action.error.message;
+      })
+      //fetch subreddits
+      .addCase(fetchSubreddits.pending, (state) => {
+        state.subreddits.status = 'loading';
+      })
+      .addCase(fetchSubreddits.fulfilled, (state, action) => {
+        state.subreddits.list = action.payload;
+        state.subreddits.status = 'succeded';
+        state.subreddits.error = null;
+      })
+      .addCase(fetchSubreddits.rejected, (state, action) => {
+        state.subreddits.status = 'failed';
+        state.subreddits.error = action.error.message;
+      })
+      //fetch comments
+      .addCase(fetchComments.pending, (state) => {
+        state.comments.status = 'loading';
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        const { postId, comments } = action.payload;
+        state.comments.byPostId[postId] = comments;
+        state.comments.status = 'pending';
+        state.comments.error = null;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.comments.status = 'failed';
+        state.comments.error = action.error.message;
       });
   },
 });
 
+export const { setSelectedSubreddit } = redditSlice.actions;
 export default redditSlice.reducer;
